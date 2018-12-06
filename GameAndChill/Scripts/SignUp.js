3a51891bb4e37f6405dbbc2cd772411320151294
Heather <harperhe@hotmail.com>
2018-12-06 16:12:26 -0500

    added Javascript

---
+++ GameAndChill/Scripts/SignUp.js
@@ -0,0 +1,17 @@
+﻿regexName = /^[A-Z][a-z]{1,19}$/;
+
+function testName() {
+    var nameValue = $('#name').val();
+    var nameError = $('nameError');
+
+    if (!regexName.test(nameValue)) {
+
+        nameError.html('Invalid!');
+        nameError.css('color', 'red');
+
+    }
+    else {
+        nameError.html('Valid!');
+        nameError.css('color','green')
+    }
+}
