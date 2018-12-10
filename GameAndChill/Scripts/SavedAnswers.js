a5f2e0264002caad71421fdd7985f8e83556c9fb
Heather <harperhe@hotmail.com>
2018-12-10 16:47:10 -0500

    tried to add cookie

---
+++ GameAndChill/Scripts/SavedAnswers.js
@@ -0,0 +1,31 @@
+﻿var saveclass = null;
+
+function saveAnswer(cookieValue) {
+    var sel = document.getElementById('AnswerSelect');
+
+    saveclass = saveclass ? saveclass : document.body.className;
+    document.body.className = saveclass + ' ' + sel.value;
+
+    setCookie('theme', cookieValue, 365);
+}
+
+function setCookie(cookieName, cookieValue, nDays) {
+    var today = new Date();
+    var expire = new Date();
+
+    if (nDays == null || nDays == 0)
+        nDays = 1;
+    expire.setTime(today.getTime() + 3600000 * 24 * nDays);
+    document.cookie = cookieName + "=" + escape(cookieValue) + ";expires=" + expire.toGMTString();
+      
+}
+
+function ReadCookie(cookieName) {
+    var theCookie = " " + document.cookie;
+    var ind = theCookie.indexOf(" " + cookieName + "=");
+    if (ind == -1) ind = theCookie.indexOf(";" + cookieName + "=");
+    if (ind == -1 || cookieName == "") return "";
+    var ind1 = theCookie.indexOf(";", ind + 1);
+    if (ind1 == -1) ind1 = theCookie.length;
+    return unescape(theCookie.substring(ind + cookieName.length + 2, ind1));
+}
